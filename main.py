02de248d97809b825187517a4a5213a0b1dc544b
Vegard Hansen Stenberg <vegard.koder@gmail.com>
2020-12-11 09:48:30 +0100

    fixing da class

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ images = []
 labels = []
 
 #index = 0
-
+"""
 def class1_label():
     labels.append("Chess")
     print(labels)
@@ -24,7 +24,8 @@ def next_img():
     canvas = tk.Canvas(root, width=300, height=300)
     canvas.pack()
     img = tk.PhotoImage(file='images\image2.png')
-    canvas.create_image(20, 20, anchor=tk.NW, image=img)
+    canvas.create_image(20, 20, image=img)
+    tk.mainloop()
 
 root = tk.Tk()
 canvas = tk.Canvas(root, width=300, height=300)
@@ -33,7 +34,7 @@ canvas.pack()
 images.extend(glob.glob('images/*.png'))
 
 img = tk.PhotoImage(file=images[0])
-canvas.create_image(20, 20, anchor=tk.NW, image=img)
+canvas.create_image(20, 20, 20, 20 image=img)
 
 class1_button = tk.Button()
 class1_button["text"] = "Chess"
@@ -46,3 +47,41 @@ class2_button["command"] = class2_label
 class2_button.pack()
 
 tk.mainloop()
+"""
+
+class App(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.labels = []
+        self.img = tk.PhotoImage(file='images\image2.png')
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.canvas = tk.Canvas(root, width=300, height=300)
+        self.canvas.pack()
+        self.canvas.create_image(20, 20, image=self.img)
+        self.canvas.image = self.img;
+
+        self.class1_button = tk.Button()
+        self.class1_button["text"] = "Chess"
+        self.class1_button["command"] = self.class1_label
+        self.class1_button.pack()
+
+        self.class2_button = tk.Button()
+        self.class2_button["text"] = "Not Chess"
+        self.class2_button["command"] = self.class2_label
+        self.class2_button.pack()
+
+    def class1_label():
+        labels.append("Chess")
+        next_img()
+
+    def class2_label():
+        labels.append("Not Chess")
+        print(labels)
+
+root = tk.Tk()
+app = App(master=root)
+app.mainloop()
